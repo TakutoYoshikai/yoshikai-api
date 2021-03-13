84e1edcafb97f6b6c947bf630d6d6e5efed37521
lucky <takuto.yoshikai@gmail.com>
2021-03-13 23:30:41 +0900

    div

--- yoshikai/yoshikai.py
+++ yoshikai/yoshikai.py
@@ -15,7 +15,7 @@ def get(site_url):
     result = []
     for work in works:
         work_title = work.find("span", {"class": "work-title"}).get_text()
-        work_detail = work.find("p", {"class": "work-detail"}).get_text().split("\n")[2].strip()
+        work_detail = work.find("div", {"class": "work-detail"}).get_text().split("\n")[2].strip()
         url = work.find("a")["href"]
         result.append({
             "title": work_title,
